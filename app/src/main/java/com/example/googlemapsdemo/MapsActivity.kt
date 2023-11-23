8364c868019f2520f0cfdfa2e21d5db58db37668
unknown <mutungaeric839@gmail.com>
2023-11-23 08:27:58 +0300

    changed the location to start at Nairobi

--- app/src/main/java/com/example/googlemapsdemo/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapsdemo/MapsActivity.kt
@@ -27,22 +27,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Nairobi, Kenya and move the camera
+        val nairobi = LatLng(-1.286389, 36.817223)
+        mMap.addMarker(MarkerOptions().position(nairobi).title("Marker in Nairobi, Kenya"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(nairobi, 12f))
     }
+
 }
